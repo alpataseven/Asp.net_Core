a6a6891b210b23b3e63b03380dd86c167f7c9665
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-01-24 03:43:16 +0300

    Update

---
+++ EntityLayer/Concrete/Portfolio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfolio
+    {
+        public int PortfolioId { get; set; }
+        public string ProjectName { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
