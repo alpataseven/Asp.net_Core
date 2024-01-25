c34ca31e51149d5cb1133af10343b25df4244e30
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-01-25 18:57:09 +0300

    Template eklendi

---
+++ netCoreProject/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
