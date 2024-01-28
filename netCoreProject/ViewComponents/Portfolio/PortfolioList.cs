3c8ccc2e0b3de6803c21482252f98ef3bdcefbd2
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-01-29 01:05:22 +0300

    Eklemeler yapıldı..

---
+++ netCoreProject/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreProject.ViewComponents.Portfolio
+{
+    public class PortfolioList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ netCoreProject/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreProject.ViewComponents.Skill
+{
+    public class SkillList : ViewComponent
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = skillManager.GetList();
+            return View(values);
+        }
+    }
+}
