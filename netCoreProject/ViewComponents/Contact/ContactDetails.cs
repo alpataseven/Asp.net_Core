124419c3a61bbb1fc8831461b1fa006da4236b07
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-02-01 03:29:40 +0300

    Contact Details and Messages

---
+++ netCoreProject/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreProject.ViewComponents.Contact
+{
+    public class ContactDetails : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ netCoreProject/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreProject.ViewComponents.Contact
+{
+    public class SendMessage : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
