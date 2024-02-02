284f5d3f2515c2b18e1e725bd19b02bb59a3969d
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-02-03 00:43:07 +0300

    Anasayfa tamamlandı..

--- netCoreProject/Controllers/DefaultController.cs
+++ netCoreProject/Controllers/DefaultController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace netCoreProject.Controllers
 {
@@ -17,5 +20,21 @@ namespace netCoreProject.Controllers
         {
             return PartialView();
         }
+
+        [HttpGet]
+        public PartialViewResult SendMessage() 
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SendMessage(Message p)
+        {
+            MessageManager messageManager = new MessageManager(new EfMessageDal());
+            p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.Status = true;
+            messageManager.TAdd(p);
+            return PartialView() ;
+        }
+
     }
 }
--- netCoreProject/ViewComponents/Contact/SendMessage.cs
+++ netCoreProject/ViewComponents/Contact/SendMessage.cs
@@ -1,14 +1,29 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace netCoreProject.ViewComponents.Contact
 {
     public class SendMessage : ViewComponent
     {
+
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+
+        [HttpGet]
         public IViewComponentResult Invoke()
         {
             return View();
         }
+
+        //[HttpPost]
+        //public IViewComponentResult Invoke(Message p)
+        //{
+        //    p.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+        //    p.Status = true;
+        //    messageManager.TAdd(p);
+        //    return View();
+        //}
 
     }
 }
