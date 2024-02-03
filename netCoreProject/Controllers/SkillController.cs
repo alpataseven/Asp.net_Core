810b0026b10110b9302a231606d9d064705c69dc
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-02-04 00:44:26 +0300

    Yetenekler güncellendi.

--- netCoreProject/Controllers/SkillController.cs
+++ netCoreProject/Controllers/SkillController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace netCoreProject.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            var values = skillManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
